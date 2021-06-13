8aca5099c9839ea30601a85ed8490a3ddbc03e69
haiip <Nikita.magnusson@live.se>
2021-06-13 23:02:59 +0200

    updating our app with a shop page, collection preview component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div>
-      <switch>
+      <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
-      </switch>
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
